cdbe32950ce4e44819324b1d35e074b0666a7cbc
Wei Tsang Ooi <weitsang@yahoo.com>
2013-01-23 17:51:25 +0800

    - modified upload script for DIY exercise - update Makefile - rename *-present.tex to *-show.tex - tested with new cf environment - new Problem Set 1

--- diy-exercises/upload.sh
+++ diy-exercises/upload.sh
@@ -1,2 +1,2 @@
-scp $1 suna:~/public_html/cs2105/1011s2/
-ssh suna chmod 644 /home/staff/ooiwt/public_html/cs2105/1011s2/$1 
+scp $1 suna:~/public_html/cs2105/1213s2/
+ssh suna chmod 644 /home/staff/ooiwt/public_html/cs2105/1213s2/$1 
